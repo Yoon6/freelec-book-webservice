353bae4e80139de7041e22725a04570de8aa1609
Daeyoon <yoonda5898@gmail.com>
2021-08-13 16:41:09 +0900

    Completes basic crud and security

--- src/main/java/com/yoon6/book/springboot/web/IndexController.java
+++ src/main/java/com/yoon6/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.yoon6.book.springboot.web;
 
+import com.yoon6.book.springboot.config.auth.LoginUser;
+import com.yoon6.book.springboot.config.auth.dto.SessionUser;
 import com.yoon6.book.springboot.service.PostsService;
 import com.yoon6.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;// 롬복 생성자에서 의존성 주입
+    private final HttpSession httpSession;
+
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -30,5 +39,6 @@ public class IndexController {
         model.addAttribute("post", dto);
         return "posts-update";
     }
+
 }
 
